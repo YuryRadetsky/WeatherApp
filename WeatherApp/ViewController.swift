380c056ea7ae96d3b014e3238faadd9fd5439912
Yury Radetsky <yura.radetsky@gmail.com>
2020-03-22 23:55:48 +0300

    add gradient background to view

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var temperatureView: UIView!
+    @IBOutlet var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Gradient background
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [UIColor(red: 145/255, green: 85/255, blue: 210/255, alpha: 1).cgColor, UIColor(red: 100/255, green: 90/255, blue: 230/255, alpha: 1).cgColor]
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
 
